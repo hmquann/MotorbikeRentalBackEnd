af41d8f601d779375e96d6edd350fed662b0cb92
hmquann <mquan171@gmail.com>
2024-08-01 15:19:05 +0700

    edit logic code for transaction

--- src/main/java/com/MotorbikeRental/dto/FilterMotorbikeDto.java
+++ src/main/java/com/MotorbikeRental/dto/FilterMotorbikeDto.java
@@ -10,9 +10,9 @@ import java.time.LocalDateTime;
 
 @Data
 public class FilterMotorbikeDto implements Serializable {
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime startDate;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime endDate;
     String address;
     Boolean isFiveStar;
