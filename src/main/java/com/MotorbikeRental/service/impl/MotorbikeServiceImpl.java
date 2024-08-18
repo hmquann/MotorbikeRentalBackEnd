fba6fc8e93f87f7042dded3eeb59a17fb03634db
Kien Nguyen <kiencbn2017@gmail.com>
2024-08-18 10:38:25 +0700

    fix filter

--- src/main/java/com/MotorbikeRental/service/impl/MotorbikeServiceImpl.java
+++ src/main/java/com/MotorbikeRental/service/impl/MotorbikeServiceImpl.java
@@ -154,12 +154,11 @@ public class MotorbikeServiceImpl  implements MotorbikeService {
 
         }
 
-
-
     @Override
     public List<Motorbike> getMotorbikeByLessorId() {
         return null;
     }
+
 
     @Override
     public List<MotorbikeDto> getAllMotorbikeByStatus(MotorbikeStatus status) {
@@ -193,7 +192,7 @@ public class MotorbikeServiceImpl  implements MotorbikeService {
         Motorbike motorbike=new Motorbike();
         motorbike.setMotorbikeAddress(registerMotorbikeDto.getMotorbikeAddress());
         motorbike.setMotorbikePlate(registerMotorbikeDto.getMotorbikePlate());
-        motorbike.setConstraintMotorbike(registerMotorbikeDto.getConstraintMotorbike());
+        motorbike.setConstraintMotorbike(registerMotorbikeDto.getConstraintMotorbike().isEmpty()?registerMotorbikeDto.getConstraintMotorbike():"Không có");
         motorbike.setModel(modelRepository.findById(registerMotorbikeDto.getModelId()).get());
         motorbike.setUser(user.get());
         motorbike.setDelivery(registerMotorbikeDto.isDelivery());
